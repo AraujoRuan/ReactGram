df4ad8e37e4d3a0674af58b980897ed0bcce9b09
Ruan <ruan_araujo20@hotmail.com>
2025-06-11 15:47:16 -0300

    Atualização do Blog

--- backend/routes/UserRoutes.jsx
+++ backend/routes/UserRoutes.jsx
@@ -2,14 +2,16 @@ const express = require("express")
 const router = express.Router()
 
 // Controller
-const { register, login } = require("../controllers/UserController.jsx")
+const { register, login, getCurrentUser } = require("../controllers/UserController.jsx")
 
 // Middleware
 const validate = require("../middlewares/handleValidation.jsx")
 const { userCreateValidation, loginValidation } = require("../middlewares/userValidations.jsx")
+const authGuard = require("../middlewares/authGuard.jsx")
 
 //Routes
 router.post("/register", userCreateValidation(), validate ,register)
 router.post("/login", loginValidation(), validate, login)
+router.get("/profile", authGuard,getCurrentUser)
 
 module.exports = router
